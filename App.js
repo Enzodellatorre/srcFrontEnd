fced0bc23426607946f79829e41f51a381c002af
Enzodellatorre <68512418+Enzodellatorre@users.noreply.github.com>
2020-08-26 19:43:31 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View,TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [valor,setValor]=useState('');
+  const[periodo,setPeriodo]=useState('');
+  const[dia,setDia]=useState('');
+  function handleSubmit(){
+    alert('valor: '+valor);
+    alert('periodo: '+periodo);
+    alert('dia: '+dia);
+  }
   return (
     <View style={styles.container}>
       <View style={styles.entradas}>
-        <TextInput placeholder="valor" keyboardType="numeric" style={styles.input}/>
-        <TextInput placeholder="periodo" keyboardType="numeric" style={styles.input}/>
-        <TextInput placeholder="dia" keyboardType="numeric" style={styles.input}/> 
+        <TextInput 
+        placeholder="valor" 
+        keyboardType="numeric"
+        value={valor}
+        onChangeText={(valor)=>setValor(valor)} 
+        style={styles.input}/>
+        <TextInput 
+        placeholder="periodo" 
+        keyboardType="numeric"
+        value={periodo}
+        onChangeText={(periodo)=>setPeriodo(periodo)}  
+        style={styles.input}/>
+        <TextInput 
+        placeholder="dia" 
+        keyboardType="numeric"
+        value={dia}
+        onChangeText={(dia)=>setDia(dia)}  
+        style={styles.input}/> 
     </View>
-       <TouchableOpacity onpress={()=>{}}>
+       <TouchableOpacity onpress={handleSubmit} style={styles.button}>
          <Text style={styles.buttonText}>Inserir valores</Text>
          </TouchableOpacity>
-       <TouchableOpacity onpress={()=>{}}>
+       <TouchableOpacity 
+       onpress={()=>{}}
+       style={styles.button}
+       >
          <Text style={styles.buttonText}>Calcular média</Text>
        </TouchableOpacity>
       <StatusBar style="auto" />
@@ -37,16 +63,17 @@ const styles = StyleSheet.create({
     marginTop:24
   },
   button:{
-   backgroundColor:"#89ffa5",
-   flexDirection:'row',
-   width:"30%",
-   height:30
-  },
-  buttonText:{
-    alignSelf:'center',
-    padding:30,
-    fontSize:30,
-    color:"#6dc4a4",
-    fontWeight:'bold'
-  },
+    justifyContent:'center',
+    alignItems:'center',
+    margin:15,
+    backgroundColor:'#41AeF4',
+    padding:10
+   },
+   buttonText:{
+     alignSelf:'center',
+     padding:30,
+     fontSize:25,
+     color:"#FFF",
+     fontWeight:'bold'
+   },
 });
