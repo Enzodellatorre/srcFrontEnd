cb6dedccb77c3c84ad22dee0d8ef0fdbaeaadea7
Enzodellatorre <68512418+Enzodellatorre@users.noreply.github.com>
2020-09-22 14:17:44 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View,TextInput, TouchableOpacity } from 'react-native';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 export default function App() {
   const [valor,setValor]=useState('');
   const[periodo,setPeriodo]=useState('');
   const[dia,setDia]=useState('');
+  const tb={
+    tableHead:['Domingo'],
+    tableTitle:['Antes do café','depois do café','antes do almoco','depois do almoco','antes do jantar', 'depois do jantar'],
+    tableData:[[90],
+               [85],
+               [93],
+               [88],
+               [92],
+               [89]
+  ]
+  }
   function handleSubmit(){
     alert('valor: '+valor);
     alert('periodo: '+periodo);
@@ -13,29 +25,38 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <View style={styles.entradas}>
-        <TextInput 
-        placeholder="valor" 
-        keyboardType="numeric"
-        value={valor}
-        onChangeText={(valor)=>setValor(valor)} 
-        style={styles.input}/>
-        <TextInput 
-        placeholder="periodo" 
-        keyboardType="numeric"
-        value={periodo}
-        onChangeText={(periodo)=>setPeriodo(periodo)}  
-        style={styles.input}/>
-        <TextInput 
-        placeholder="dia" 
-        keyboardType="numeric"
-        value={dia}
-        onChangeText={(dia)=>setDia(dia)}  
-        style={styles.input}/> 
-    </View>
+      <Table borderStyle={{borderWidth:1}}>
+                <Row data={tb.tableHead} flexArr={[1,2,1,1]}style={styles.head} textStyle={styles.text}/>
+                <TextInput 
+                          placeholder="dia" 
+                          keyboardType="numeric"
+                          value={dia}
+                          onChangeText={(dia)=>setDia(dia)}  
+                          style={styles.input}/>
+                <TableWrapper style={styles.wrapper}>
+                    <Col data={tb.tableTitle} style={styles.title} heightArr={[28,28]}textStyle={styles.text}/>
+                    <TextInput 
+                          placeholder="periodo" 
+                          keyboardType="numeric"
+                          value={periodo}
+                          onChangeText={(periodo)=>setPeriodo(periodo)}  
+                          style={styles.input}/>
+                    
+                    <Rows data={tb.tableData} flexArr={[2,1,1]} style={styles.row} textStyle={styles.text}/>
+                    <TextInput 
+                              placeholder="valor" 
+                              keyboardType="numeric"
+                              value={valor}
+                              onChangeText={(valor)=>setValor(valor)} 
+                              style={styles.input}/>
+                </TableWrapper>
+            </Table>
+      
+       
        <TouchableOpacity onpress={handleSubmit} style={styles.button}>
          <Text style={styles.buttonText}>Inserir valores</Text>
          </TouchableOpacity>
+       
        <TouchableOpacity 
        onpress={()=>{}}
        style={styles.button}
@@ -52,16 +73,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF'
   },
-  entradas:{
+  head:{
+    height:40,
+    backgroundColor:'#f1f8ff'
+},
+wrapper:{
     flexDirection:'row'
-  },
-  input:{
-    height:80,
-    textAlign:'center',
-    width:"30%",
-    fontSize:30,
-    marginTop:24
-  },
+},
+title:{
+    flex:1,
+    backgroundColor:'#f6f8fa'
+},
+row:{
+    height:28
+},
+text:{
+    textAlign:'center'
+},
   button:{
     justifyContent:'center',
     alignItems:'center',
@@ -75,5 +103,5 @@ const styles = StyleSheet.create({
      fontSize:25,
      color:"#FFF",
      fontWeight:'bold'
-   },
+   }
 });
