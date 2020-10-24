7e84b7fdb1196024ec340d7d5cb765b2a3cf9c47
Enzodellatorre <68512418+Enzodellatorre@users.noreply.github.com>
2020-10-24 15:43:17 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View,TextInput, TouchableOpacity } from 'react-native';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
-
+import SlidingPanel from 'react-native-sliding-panels';
 export default function App() {
   const [valor,setValor]=useState('');
   const[periodo,setPeriodo]=useState('');
@@ -23,6 +23,38 @@ export default function App() {
     alert('periodo: '+periodo);
     alert('dia: '+dia);
   }
+  fetch('localhost:3000/nivel',
+  {
+    method:'POST',
+    headers:{
+      Accept:'application/json',
+      'Content-type':'application-json'
+    },
+    body:JSON.stringify({
+      firstParam:'valorNivel',
+      secondParam:'idPeriodo',
+      thirdParam:'idDia'
+    })
+  });
+  const getNiveisFromApiAsync=async ()=>{
+    try{
+      let response=await fetch('localhost:3000/niveis');
+    let json=await response.json();
+    return json.niveis;
+    }catch(error){
+      console.error(error);
+    }
+  }
+  /*
+  function calcularMedia({{tableData}}){
+    var media=0;
+    var soma=0;
+    forEach(numero){
+      soma+= numero;
+      media=(soma)/6;
+      return media;
+    };
+  */
   return (
     <View style={styles.container}>
       <Table borderStyle={{borderWidth:1}}>
@@ -33,6 +65,7 @@ export default function App() {
                           value={dia}
                           onChangeText={(dia)=>setDia(dia)}  
                           style={styles.input}/>
+                
                 <TableWrapper style={styles.wrapper}>
                     <Col data={tb.tableTitle} style={styles.title} heightArr={[28,28]}textStyle={styles.text}/>
                     <TextInput 
@@ -43,15 +76,23 @@ export default function App() {
                           style={styles.input}/>
                     
                     <Rows data={tb.tableData} flexArr={[2,1,1]} style={styles.row} textStyle={styles.text}/>
-                    <TextInput 
+                    <TextInput
                               placeholder="valor" 
                               keyboardType="numeric"
                               value={valor}
                               onChangeText={(valor)=>setValor(valor)} 
                               style={styles.input}/>
+                              
                 </TableWrapper>
             </Table>
-      
+      <SlidingPanel
+      headerLayoutHeight={100}
+      headerLayout={()=>
+      <View style={styles.headerLayoutStyle}>
+          <Text style={styles.text}>{tb.tableHead}</Text>
+      </View>
+      }
+      />
        
        <TouchableOpacity onpress={handleSubmit} style={styles.button}>
          <Text style={styles.buttonText}>Inserir valores</Text>
@@ -76,6 +117,11 @@ const styles = StyleSheet.create({
   head:{
     height:40,
     backgroundColor:'#f1f8ff'
+},
+input:{
+  alignSelf:'right',
+  alignItems:'right',
+  padding:10
 },
 wrapper:{
     flexDirection:'row'
@@ -103,5 +149,19 @@ text:{
      fontSize:25,
      color:"#FFF",
      fontWeight:'bold'
-   }
+   },
+   headerLayoutStyle: {
+    width:100, 
+    height: 100, 
+    backgroundColor: 'orange', 
+    justifyContent: 'center', 
+    alignItems: 'center',
+  },
+  slidingPanelLayoutStyle: {
+    width:100, 
+    height:100, 
+    backgroundColor: '#7E52A0', 
+    justifyContent: 'center', 
+    alignItems: 'center',
+  }
 });
